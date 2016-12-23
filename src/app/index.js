545b5a661af2ca3bda6eb18ce589957193e751da
JackYin <1259559060@qq.com>
2016-12-23 13:52:48 +0800

    test

--- src/app/index.js
+++ src/app/index.js
@@ -2,7 +2,7 @@
  * @Author: yinjikai
  * @Date: 2016-12-22 13:47:20
  * @Last Modified by: yinjikai
- * @Last Modified time: 2016-12-23 11:07:54
+ * @Last Modified time: 2016-12-23 12:14:51
  */
 import './main.scss'
 import React, { Component } from 'react'
@@ -10,7 +10,7 @@ import { render } from 'react-dom'
 class Hello extends Component {
     render() {
         return (
-            <h1>hello webpack</h1>
+            <h1>hello  webpack</h1>
         )
     }
 }
